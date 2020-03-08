5934a827a77846837b0974c559b5839040239deb
James Canada <jamesccanada@gmail.com>
2020-03-08 12:28:11 -0500

    blend works

---
+++ ControlAvatar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlAvatar : MonoBehaviour
+{
+    public Animator a;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a.SetFloat("Forward", (Input.GetAxis("Vertical")));
+        a.SetFloat("Turn", Input.GetAxis("Horizontal"));
+    }
+}
